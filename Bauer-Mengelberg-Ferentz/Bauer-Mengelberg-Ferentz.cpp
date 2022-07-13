5377eae2148846865c6490d587e59f1ceff9ca98
fleximeter <59806547+fleximeter@users.noreply.github.com>
2022-07-13 15:57:24 -0400

    Documentation

--- Bauer-Mengelberg-Ferentz/Bauer-Mengelberg-Ferentz.cpp
+++ Bauer-Mengelberg-Ferentz/Bauer-Mengelberg-Ferentz.cpp
@@ -1,5 +1,26 @@
-// Bauer-Mengelberg-Ferentz.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+* File: Bauer-Mengelberg-Ferentz.cpp
+* Author: Jeff Martin
+* This file contains the main() function for running the Bauer-Mengelberg-Ferentz
+* algorithm for finding eleven-interval row generators.
+*
+* Copyright © 2022 by Jeffrey Martin. All rights reserved.
+* Email: jmartin@jeffreymartincomposer.com
+* Website: https://jeffreymartincomposer.com
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #include "bauer_algorithm.h"
 
@@ -8,14 +29,3 @@ int main()
 	generateAllIntervalRows("D:\\eleven_interval.json");
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- Bauer-Mengelberg-Ferentz/bauer_algorithm.h
+++ Bauer-Mengelberg-Ferentz/bauer_algorithm.h
@@ -1,3 +1,28 @@
+/*
+* File: bauer-algorithm.h
+* Author: Jeff Martin
+* 
+* This file contains functionality for running the Bauer-Mengelberg-Ferentz
+* algorithm for finding eleven-interval row generators.
+*
+* Copyright © 2022 by Jeffrey Martin. All rights reserved.
+* Email: jmartin@jeffreymartincomposer.com
+* Website: https://jeffreymartincomposer.com
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef BAUER
 #define BAUER
 #include <fstream>
