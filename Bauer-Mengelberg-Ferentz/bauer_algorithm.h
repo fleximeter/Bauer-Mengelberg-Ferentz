59adde792431bde6604e37cda35f8f8a05a88e59
fleximeter <59806547+fleximeter@users.noreply.github.com>
2022-07-13 14:46:17 -0400

    finalized

--- Bauer-Mengelberg-Ferentz/bauer_algorithm.h
+++ Bauer-Mengelberg-Ferentz/bauer_algorithm.h
@@ -5,8 +5,8 @@
 #include <string>
 
 void generateAllIntervalRows(std::string destinationFile);
-bool isValidPermutation(int* permutation);
-void nextPermutation(int* permutation, int* buffer);
+int isValidPermutation(int* permutation);
+void nextPermutation(int* permutation);
 void writeToFile(std::string path, int** found, int numFound);
 
 /// <summary>
@@ -15,39 +15,40 @@ void writeToFile(std::string path, int** found, int numFound);
 void generateAllIntervalRows(std::string destinationFile)
 {
 	int permutation[11] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-	int buffer[11] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
 	int numFound = 0;
 	int sizeFound = 256;
 	int** found = new int* [sizeFound];
 
 	std::cout << "Starting...\n";
+	nextPermutation(permutation);
 
 	// consider each possible permutation
 	while (permutation[0] < 6)
 	{
-		if (isValidPermutation(permutation))
+		// if it is time to resize the array of found row generators,
+		// resize it
+		if (numFound >= sizeFound)
 		{
-			// if it is time to resize the array of found row generators,
-			// resize it
-			if (numFound >= sizeFound)
-			{
-				int** newFound = new int* [sizeFound * 2];
-				for (int i = 0; i < numFound; i++)
-					newFound[i] = found[i];
-				delete[] found;
-				found = newFound;
-				sizeFound *= 2;
-			}
-
-			// add the new generator to the array
-			found[numFound] = new int[11];
-			for (int i = 0; i < 11; i++)
-				found[numFound][i] = permutation[i];
-			numFound++;
+			int** newFound = new int* [sizeFound * 2];
+			for (int i = 0; i < numFound; i++)
+				newFound[i] = found[i];
+			delete[] found;
+			found = newFound;
+			sizeFound *= 2;
 		}
 
+		// add the new generator to the array
+		found[numFound] = new int[11];
+		for (int i = 0; i < 11; i++)
+			found[numFound][i] = permutation[i];
+		numFound++;
+
+		std::cout << "Found " << numFound << " row generators.\n";
+		if (numFound > 5000)
+			break;
+
 		// move to the next permutation
-		nextPermutation(permutation, buffer);
+		nextPermutation(permutation);
 	}
 
 	// export the row generators
@@ -65,10 +66,11 @@ void generateAllIntervalRows(std::string destinationFile)
 /// Determines if a permutation is valid
 /// </summary>
 /// <param name="permutation">A permutation</param>
-/// <returns>Whether or not it is valid</returns>
-bool isValidPermutation(int* permutation)
+/// <returns>The index of the partial sum that failed, or -1 if the permutation is valid</returns>
+int isValidPermutation(int* permutation)
 {
 	int sums[12] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+	int returnVal = -1;
 	for (int i = 0; i < 11; i++)
 	{
 		int sum = 0;
@@ -76,54 +78,114 @@ bool isValidPermutation(int* permutation)
 			sum += permutation[j];
 		sum %= 12;
 		if (sum == 0)
-			return false;
+			return i;
 		else if (i < 10 && sum == 6)
-			return false;
-		else if (sums[sum] > 0)
-			return false;
-		else
-			sums[sum] = sum;
+			return i;
+		else if (sums[sum] > -1)
+			returnVal = 11;
+		sums[sum] = sum;
 	}
-	return true;
+	return returnVal;
 }
 
 /// <summary>
 /// Updates the permutation to the next valid one
 /// </summary>
 /// <param name="permutation">The permutation</param>
-/// <param name="buffer">A buffer for sorting numbers</param>
-void nextPermutation(int* permutation, int* buffer)
+/// <param name="criticalIndex">The critical index, if it is known</param>
+void nextPermutation(int* permutation)
 {
-	int smallestRight = 0;
-	int criticalIndex = -1;
-	buffer[permutation[10]] = permutation[10];
+	// assume that the permutation is valid
+	int validPermutation = -1;
 
-	// Identify the index of the critical digit, as well as the smallest number
-	for (int i = 9; i >= 0; i--)
+	do
 	{
-		buffer[permutation[i]] = permutation[i];
-		if (permutation[i] < permutation[i + 1])
+		int criticalIndex = validPermutation;
+		int smallestRight = 0;
+		int buffer[12] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+		buffer[permutation[10]] = permutation[10];
+
+		// if we don't have a critical index already, we need to find it
+		if (criticalIndex == -1 || criticalIndex > 10)
 		{
-			smallestRight = permutation[i] + 1;
-			criticalIndex = i;
+			// Identify the index of the critical digit, as well as the smallest number
+			// Critical digit cannot be the last digit; must be index 9 or lower
+			for (int i = 9; i >= 0; i--)
+			{
+				buffer[permutation[i]] = permutation[i];
+
+				// if we have identified the critical digit
+				if (permutation[i] < permutation[i + 1])
+				{
+					smallestRight = permutation[i] + 1;
+					criticalIndex = i;
+					while (buffer[smallestRight] == -1)
+						smallestRight++;
+					permutation[i] = smallestRight;
+					buffer[smallestRight] = -1;
+					break;
+				}
+			}
+		}
+
+		// If the critical index was provided
+		else
+		{
+			for (int i = 9; i >= criticalIndex; i--)
+				buffer[permutation[i]] = permutation[i];
+			smallestRight = permutation[criticalIndex] + 1;
+			// If the critical digit is not 11, search for the smallest right
+			// digit that is larger than the critical digit
+			if (smallestRight < 12)
+			{
+				while (buffer[smallestRight] == -1)
+				{
+					smallestRight++;
+					if (smallestRight > 11)
+						break;
+				}
+			}
+			// If we were unable to find an acceptable smallest right digit,
+			// we need to find a new critical digit.
+			if (smallestRight > 11)
+			{
+				// Identify the index of the critical digit, as well as the smallest number
+				// Critical digit cannot be the last digit; must be index 9 or lower
+				for (int i = criticalIndex - 1; i >= 0; i--)
+				{
+					buffer[permutation[i]] = permutation[i];
+
+					// if we have identified the critical digit
+					if (permutation[i] < permutation[i + 1])
+					{
+						smallestRight = permutation[i] + 1;
+						criticalIndex = i;
+						while (buffer[smallestRight] == -1)
+							smallestRight++;
+						permutation[i] = smallestRight;
+						buffer[smallestRight] = -1;
+						break;
+					}
+				}
+			}
+			else
+			{
+				permutation[criticalIndex] = smallestRight;
+				buffer[smallestRight] = -1;
+			}
+		}
+
+		// Update the permutation and reset the buffer
+		smallestRight = 0;
+		for (int i = criticalIndex + 1; i < 11; i++)
+		{
 			while (buffer[smallestRight] == -1)
 				smallestRight++;
 			permutation[i] = smallestRight;
-			buffer[smallestRight] = -1;
-			smallestRight = 0;
-			break;
-		}
-	}
-
-	// Update the permutation and reset the buffer
-	for (int i = criticalIndex; i < 11; i++)
-	{
-		while (buffer[smallestRight] == -1 && smallestRight < 11)
 			smallestRight++;
-		permutation[i] = smallestRight;
-		buffer[smallestRight] = -1;
-		smallestRight++;
-	}
+		}
+		validPermutation = isValidPermutation(permutation);
+	} while (validPermutation > -1);
 }
 
 /// <summary>
@@ -134,7 +196,7 @@ void writeToFile(std::string path, int** found, int numFound)
 {
 	std::ofstream file;
 	file.open("D:\\eleven_interval.json");
-	file << "{\n    \"elevenIntervalRows\": [\n";
+	file << "{\n    \"elevenIntervalRowGenerators\": [\n";
 	for (int i = 0; i < numFound - 1; i++)
 	{
 		file << "        [";
